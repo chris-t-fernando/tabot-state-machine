cabae756c90e5629aa5a9c12f1cf824535e20754
Chris Fernando <chris.t.fernando@gmail.com>
2022-11-26 22:46:55 +1100

    added paramters

--- sqs_to_rds/sqs_to_rds.py
+++ sqs_to_rds/sqs_to_rds.py
@@ -2,6 +2,7 @@ import boto3
 import time
 import json
 import mysql.connector
+from parameter_store import Ssm
 
 # need to add a unique ID per instance
 # need to add the datetime that the instance started and ended
@@ -10,6 +11,7 @@ def insert_instance_terminated(
     average_sell_price,
     bought_value,
     buy_order_count,
+    instance_id,
     play_config_name,
     run_id,
     sell_order_count,
@@ -20,8 +22,10 @@ def insert_instance_terminated(
     total_gain,
     units,
     weather_condition,
+    **kwargs,
 ):
     vals = (
+        instance_id,
         average_buy_price,
         average_sell_price,
         bought_value,
@@ -37,14 +41,14 @@ def insert_instance_terminated(
         units,
         weather_condition,
     )
-    sql = "insert into instance_results (average_buy_price, average_sell_price, bought_value, buy_order_count, play_config_name, run_id, sell_order_count, sell_order_filled_count, sold_value, symbol, symbol_group, total_gain, units, weather_condition) VALUES (%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s, %s, %s)"
+    sql = "insert into instance_results (instance_id, average_buy_price, average_sell_price, bought_value, buy_order_count, play_config_name, run_id, sell_order_count, sell_order_filled_count, sold_value, symbol, symbol_group, total_gain, units, weather_condition) VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s, %s, %s)"
     try:
         mycursor.execute(sql, vals)
     except mysql.connector.IntegrityError as e:
         if "Duplicate entry" not in e.msg:
             raise
     else:
-        print(f"New instance result: \t\t{run_id} symbol {symbol}")
+        print(f"New instance result: \t\t{run_id} instance {instance_id}")
 
 
 def insert_play_orchestrator(
@@ -61,16 +65,17 @@ def insert_play_orchestrator(
         print(f"New Play Orchestrator: \t\t{play_id}")
 
 
-sqs_client = boto3.client("sqs")
-sqs_queue_url = (
-    "https://sqs.ap-southeast-2.amazonaws.com/036372598227/tabot_backtest.fifo"
-)
+store = Ssm()
 
+sqs_client = boto3.client("sqs")
+sqs_queue_url = store.get("/tabot/telemetry/queue/backtest")
+
+_base = "/tabot/telemetry/sql/"
 mydb = mysql.connector.connect(
-    host="jtweets.ciizausrav91.us-west-2.rds.amazonaws.com",
-    user="jtweets",
-    password="jtweets1",
-    database="tabot_results",
+    host=store.get(f"{_base}host"),
+    user=store.get(f"{_base}user"),
+    password=store.get(f"{_base}password"),
+    database=store.get(f"{_base}database"),
 )
 mycursor = mydb.cursor()
 
@@ -91,10 +96,18 @@ while True:
             body = m["Body"]
             body_json = json.loads(body)
 
-            if body_json["event"] == "Play start":
+            event = body_json["event"].lower()
+            if event == "play start":
                 insert_play_orchestrator(**body_json)
-            elif body_json["event"] == "Instance terminated":
+            elif event == "instance terminated":
                 insert_instance_terminated(**body_json)
 
         mydb.commit()
-    time.sleep(2)
+        sqs_client.delete_message_batch(
+            QueueUrl=sqs_queue_url,
+            Entries=[
+                {"Id": d["MessageId"], "ReceiptHandle": d["ReceiptHandle"]}
+                for d in messages["Messages"]
+            ],
+        )
+    # time.sleep(2)
